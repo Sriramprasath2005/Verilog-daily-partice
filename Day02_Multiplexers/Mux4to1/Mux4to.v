2349dcb1c0cccea00a650984bf1e0a56c152d54d
Sriramprasath2005 <sriramgithub31@gmail.com>
2026-08-03 21:40:45 +0530

    Update Mux4to.v

--- Day02_Multiplexers/Mux4to1/Mux4to.v
+++ Day02_Multiplexers/Mux4to1/Mux4to.v
@@ -1,1 +1,12 @@
-
+module mux4to1(input I0,I1,I2,I3,input [1:0]S, output reg y);
+  
+ 
+  always @(*) begin
+    case (S)
+      2'b00: y = I0;
+      2'b01: y = I1;
+      2'b10: y = I2;
+      2'b11: y = I3;
+    endcase
+  end
+endmodule
